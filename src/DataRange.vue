52b5f116d6a968e981bb645bb08e49f91cdce20e
Igor Stcherbina <orel1939@mail.ru>
2017-02-24 10:08:58 +0200

    Ready.

--- src/DataRange.vue
+++ src/DataRange.vue
@@ -1,31 +1,14 @@
 <template>
 	<div id="data-range" v-show="range.length">
-		<div class="labels" >
-			<span
+		<ul class="labels" >
+			<li
 				v-for="(part, inx) in range"
 				v-html="part.label"
-				:style="styleLabel()"
+				:data-num="inx"
 				:class ='classLabel(part, inx)'
-				:data-num="inx"
-				></span>
-		</div>
-		<div class="scale">
-			<span
-				v-for="(part, inx) in range"
-				:style="stylePeriod()"
-				:class ='classPeriod(part, inx)'
-				:data-num="inx"
-				></span>
-		</div>
-		<input type="range" class="input"
-			v-model="val"
-			:style="styleInput()"
-			:min="min"
-			:max="max"
-			:value="val"
-			:step="step"
-			@change= 'valChange()'
-			>
+			></li>
+		</ul>
+		<div class="input" id="slider"></div>
 	</div>
 </template>
 
@@ -46,9 +29,6 @@ export default {
 	},
 	data () {
 		return {
-			widthSteep : 0,
-			isAllEmpty : false,
-			isFirst : 0,
 			range : [],
 			step : 1,
 			min : 0,
@@ -58,47 +38,14 @@ export default {
 	},
 	methods : {
 		/**
-		 * Find first no empty element.
-		 */
-		valFindNoEmpty () {
-			let count = 1;
-			let that = this;
-			let shift = that.val;
-			let isNext = true;
-			let len = that.range.length;
-
-			that.isAllEmpty = false;
-
-			while (isNext && count < len) {
-				shift++;
-
-				if (shift === len ) {
-					shift = 0;
-				}
-
-				let next = that.range[shift];
-
-				if (next && !next.isEmpty) {
-					isNext = false;
-					that.val = shift;
-					return that.valChangeAfterHook();
-				}
-
-				count++;
-			}
-			that.val = that.isFirst;
-			that.isAllEmpty =  true;
-			that.valChangeAfterHook();
-
-		},
-		/**
 		 * Hook after change value.
 		 */
 		valChangeAfterHook() {
 			let that = this;
+			let w  = window;
 
-			if (window.vueEvents) {
-				window.vueEvents.$emit('dataRangeChangeValue',  {
+			if (w.vueEvents) {
+				w.vueEvents.$emit('dataRangeChangeValue',  {
 					val  : that.val,
 					data : that.range[that.val]
 				});
@@ -110,17 +57,6 @@ export default {
 		 */
 		valChange () {
 			let that = this;
-
-			if (that.isAllEmpty) {
-				that.val = that.isFirst;
-				return;
-			}
-
-			let p = that.range[that.val];
-
-			if (p.isEmpty) {
-				that.valFindNoEmpty();
-			}
 
 			that.valChangeAfterHook();
 		},
@@ -136,40 +72,10 @@ export default {
 
 			return c;
 		},
-		/**
-		 * Get class for period.
-		 */
-		classPeriod (data, num) {
-			let c = 'period';
-
-			if (data.isEmpty) {
-				c += ' empty';
-			}
-
-			if (this.val == num) {
-				c += ' active';
-			}
-
-			return c;
-		},
-		/**
-		 * Return style for labels.
-		 */
-		styleLabel() {
-			return `width: ${this.widthLabel}px;`
-		},
-		/**
-		 * Return styke for input range.
-		 */
-		styleInput() {
-			return `width: ${this.width}px;`
-		},
-		stylePeriod () {
-			return `width: ${this.widthSteep}px;`
-		}
 	},
 	created() {
 		let that = this;
+		let w = window;
 
 		if (that.dataRange.length) {
 			that.range = that.dataRange;
@@ -181,7 +87,6 @@ export default {
 
 			if (p.isFirst) {
 				that.val = i;
-				that.isFirst = i;
 				break;
 			}
 		}
@@ -192,15 +97,21 @@ export default {
 			that.max = 0;
 		}
 
-		if (that.range[that.val].isEmpty) {
-			that.valFindNoEmpty();
-		}
+		that.percent = 100 / that.max;
 
-		if (window.vueEvents) {
-			window.vueEvents.$on('cartogramsDataRangeUpdate', function () {
-				if (that.range[that.val].isEmpty) {
-					that.valFindNoEmpty();
-				}
+		if (w.vueEvents) {
+			w.vueEvents.$on('cartogramsDataRangeUpdate', function () {
+
+				that.range.map((data, inx) => {
+					let $el = $('#year_' + inx);
+					let cls = 'empty';
+					
+					$el.removeClass(cls);
+
+					if (data && data.isEmpty) {
+						$el.addClass(cls);
+					}
+				});
 
 				that.$forceUpdate();
 			});
@@ -209,31 +120,47 @@ export default {
 	},
 	mounted () {
 		let that = this;
-		that.width = $('#data-range').width();
-		that.widthSteep = that.width / (that.range.length - 1);
-		let pos = $('#data-range > .scale').position();
+		let labelWidth = that.percent * $('#data-range').width() / 100;
+		let $range = $('#data-range > .input').slider({
+			value : that.val,
+			step  : that.step,
+			min   : that.min,
+			max   : that.max,
+			slide : (ev, ui) =>  {
+				if (that.range[ui.value] || that.range[ui.value].isEmpty) {
+					return false;
+				}
 
-		$('.data-range-labels').each(function() {
-			let $el = $(this);
-			let num = $el.data('num');
-
-			$el.offset({
-				top : pos.top - 40,
-				left : ((pos.left - that.widthLabel/ 2) + that.widthSteep * num)
-			});
+				that.val = ui.value;
+			}
 		});
+		let $labels = $('#data-range > .labels');
+		$labels.width($labels.width() + labelWidth);
+		$labels.find('li').width(labelWidth);
 
-		$('#data-range > .scale > .period').each(function() {
-			let $el = $(this);
-			let num = $el.data('num');
+		let cls = 'years';
 
-			$el.offset({
-				top : pos.top + 3,
-				left : ((pos.left - that.widthLabel ) + that.widthSteep * num)
-			});
-		});
+		let addYear = (isEmpty, num, isAfter) => {
+			let call = (isAfter || false) ? 'after' :  'append';
+			let setClass = cls;
 
-		that.$forceUpdate();
+			if (isEmpty) {
+				setClass += ' empty'
+			}
+
+			$range[call]($('<div>', {
+				id : 'year_' + num,
+				class : setClass,
+				style : `width: ${that.percent}%;height: 10px;`
+			}));
+		}
+
+		for (var i = 0; i< that.max ; ++i) {
+			addYear(that.range[i].isEmpty, i);
+		}
+
+		addYear(that.range[i].isEmpty, i, true);
+
 	}
 }
 </script>
